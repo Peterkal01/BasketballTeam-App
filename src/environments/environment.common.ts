7b82a12d2127b18b99b16065ad564c0214f5ecbd
Peterkal01 <48078237+Peterkal01@users.noreply.github.com>
2020-12-03 09:47:08 +1100

    initial commit

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCnKuh5rDMdQvhiYlE0_m_QuAOxmynglLw',
+    authDomain: 'basketballteam-app.firebaseapp.com',
+    projectId: 'basketballteam-app',
+    storageBucket: 'basketballteam-app.appspot.com',
+    messagingSenderId: '298041676679',
+    appId: '1:298041676679:web:8ed8e19985c438b36f7922',
+  },
+};
